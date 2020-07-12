c0de8604b98041502d00997136db683ac60a49a1
Miguel Dummer <darckluso@gmail.com>
2020-07-12 08:33:40 -0300

    DecimalComparator exercise

---
+++ src/DecimalComparator.java
@@ -0,0 +1,12 @@
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces (double x, double y){
+
+        int xInt = (int) (x * 1000);
+        int yInt = (int) (y * 1000);
+
+        return xInt == yInt;
+
+    }
+    
+}
