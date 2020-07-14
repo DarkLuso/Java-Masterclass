f13653ba1f450cc927db969892a95a6aea1ab49f
Miguel Dummer <darckluso@gmail.com>
2020-07-14 19:12:18 -0300

    PaintJob exercise

---
+++ src/PaintJob.java
@@ -0,0 +1,36 @@
+public class PaintJob {
+
+    public static int getBucketCount (double width, double height, double areaPerBucket, int extraBuckets){
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0)
+            return -1;
+
+        double area = height * width;
+        double areaPossible = areaPerBucket * extraBuckets;
+        double areaLeft = area - areaPossible;
+
+        return (int)Math.ceil(areaLeft / areaPerBucket);
+
+    }
+
+    public static int getBucketCount (double width, double height, double areaPerBucket){
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0)
+            return -1;
+
+        double area = height * width;
+
+        return (int)Math.ceil(area / areaPerBucket);
+
+    }
+
+    public static int getBucketCount (double area, double areaPerBucket){
+
+        if (area <= 0 || areaPerBucket <= 0)
+            return -1;
+
+        return (int)Math.ceil(area / areaPerBucket);
+
+    }
+
+}
